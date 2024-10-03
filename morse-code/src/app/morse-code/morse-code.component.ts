7d46b7523294eeb2a38d5ba2c678a8fe2576e436
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-10-03 22:37:44 +0200

    generated angular app for project

---
+++ morse-code/src/app/morse-code/morse-code.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-morse-code',
+  standalone: true,
+  imports: [],
+  templateUrl: './morse-code.component.html',
+  styleUrl: './morse-code.component.css'
+})
+export class MorseCodeComponent {
+
+}
